f8120074a5eb4782eb4a4365058c46757d6c3bcd
Michael Lajlev <lajlev@gmail.com>
2021-09-09 12:47:17 +0200

    add link to gps-coordinates.net

--- resources/views/attractions/create.blade.php
+++ resources/views/attractions/create.blade.php
@@ -35,6 +35,7 @@
           <div class="form-group">    
             <label for="image">Latitude</label>
             <input type="text" class="form-control" name="latitude" placeholder="fx. 55.12345">
+            <div class="form-text">Convert an address to coordinates on <a href="https://www.gps-coordinates.net/">gps-coordinates.net</a></div>
         </div>  
           <div class="form-group">    
               <label for="image">Longitude</label>
